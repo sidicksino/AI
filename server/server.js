f47f22ea2a9f1016c916e8a853c59c345405a111
sidicksino <sidickabdoulayesino1@gmail.com>
2025-08-26 01:56:49 +0200

    Fixed Stripe error

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cors())
 app.use(express.json())
 
 //  Routes 
-app.get('/', (req, res) => {res.send('Server on live!')})
+app.get('/', (req, res) => {res.send('Server running!')})
 app.use('/api/user', userRouter)
 app.use('/api/chat', chatRouter)
 app.use('/api/message', messageRouter)

f08afbf981fde1a51d21ae11aef1f14a1510806b
sidicksino <sidickabdoulayesino1@gmail.com>
2025-08-26 01:56:17 +0200

    Fixed Stripe error

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -21,7 +21,7 @@ export const stripeWebhooks = async (request, response) => {
                     payment_intent: paymentIntent.id,
                 })
                 const session = sessionList.data[0];
-                const {transactionId, appId} = session.metadat;
+                const {transactionId, appId} = session.metadata;
 
                 if(appId === 'sino_ai'){
                     const transaction = await Transaction.findOne({_id: transactionId, isPaid: false})

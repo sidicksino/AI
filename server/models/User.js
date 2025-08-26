19c005133ff506aaf2f1f4655c7f786df756fcdd
sidicksino <sidickabdoulayesino1@gmail.com>
2025-08-26 02:08:48 +0200

    Change staus from User schama to credits

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    status: { type: Number, default: 20 },
+    credits: { type: Number, default: 20 },
 })
 
 // Hash password before saving to database
